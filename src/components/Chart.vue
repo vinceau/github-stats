a02dfad987e0c95699b7ffac930372a40ecd6ad9
Vince Au <vinceau09@gmail.com>
2020-06-05 22:11:45 +1000

    Add Chart component

---
+++ src/components/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chart">
+    <apexchart
+      type="line"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import VueApexCharts from "vue-apexcharts";
+
+@Component({
+  components: {
+    apexchart: VueApexCharts,
+  },
+})
+export default class Chart extends Vue {
+  @Prop() private id!: string;
+  @Prop() private series!: any[];
+
+  private chartOptions = {
+    chart: {
+      id: this.id,
+    },
+    xaxis: {
+      type: "datetime",
+    },
+  };
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <apexchart
-      type="line"
-      height="350"
-      :options="chartOptions"
-      :series="series"
-    />
+    <chart :id="chartId" :series="series" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Chart from "@/components/Chart.vue";
 
-import VueApexCharts from "vue-apexcharts";
 import releaseData from "../data.json";
 
 const downloadData = releaseData[0].releaseAssets.nodes.map(r => {
@@ -27,23 +22,15 @@ const downloadData = releaseData[0].releaseAssets.nodes.map(r => {
 
 console.log(downloadData);
 
-
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    apexchart: VueApexCharts,
+    chart: Chart,
   },
   data() {
     return {
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          type: "datetime",
-        },
-      },
+      chartId: "vuechart-example",
       series: downloadData,
     };
   },
