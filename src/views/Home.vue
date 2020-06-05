da5a52df62e2dab5797b2816a9eebd9618327e52
Vince Au <vinceau09@gmail.com>
2020-06-05 18:50:24 +1000

    Working line graph

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <apexchart
+      type="line"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    />
   </div>
 </template>
 
@@ -9,10 +15,31 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+import VueApexCharts from "vue-apexcharts";
+
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  },
 };
 </script>
