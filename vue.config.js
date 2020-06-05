48b21ddb124aaaba7010b5cac88ab2da6651bb6c
Vince Au <vinceau09@gmail.com>
2020-06-06 01:38:07 +1000

    Determine repo name from package.json

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require("./package.json");
+
 module.exports = {
-  publicPath: "/github-stats/",
+  publicPath: process.env.NODE_ENV === "production" ? `/${pkg.name}/` : "/",
 };
